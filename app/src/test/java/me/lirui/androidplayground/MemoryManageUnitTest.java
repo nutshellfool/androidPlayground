a6d3f4b1676b973462cf242384f5b0be8b215ca3
李瑞 <lirui.sep@gmail.com>
2017-05-12 15:27:24 +0800

    Init commit

---
+++ app/src/test/java/me/lirui/androidplayground/MemoryManageUnitTest.java
@@ -0,0 +1,23 @@
+package me.lirui.androidplayground;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Created by RichardLee on 2017/5/12.
+ */
+
+public class MemoryManageUnitTest {
+    @Test
+    public void strongReferenceTest() throws Exception {
+        Object object = new Object();
+        Object otherObject = object;
+        Assert.assertNotNull(object);
+        Assert.assertNotNull(otherObject);
+
+        object = null;
+        Assert.assertNull(object);
+        Assert.assertNotNull(otherObject);
+    }
+}
