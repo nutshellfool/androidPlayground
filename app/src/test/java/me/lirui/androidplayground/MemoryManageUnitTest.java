40d07b062c564e4aa9010a3005c5495d7debb512
李瑞 <lirui.sep@gmail.com>
2017-05-17 21:52:02 +0800

    And weak reference & soft reference test case

--- app/src/test/java/me/lirui/androidplayground/MemoryManageUnitTest.java
+++ app/src/test/java/me/lirui/androidplayground/MemoryManageUnitTest.java
@@ -3,6 +3,9 @@ package me.lirui.androidplayground;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
+
 
 /**
  * Created by RichardLee on 2017/5/12.
@@ -23,7 +26,28 @@ public class MemoryManageUnitTest {
 
     @Test
     public void weakReferenceTest() throws Exception {
-//        ActivityManager.getMemoryClass()
+        WeakReference r = new WeakReference(new String("I'm here"));
+        WeakReference sr = new WeakReference("I'm here");
+        Assert.assertNotNull(r.get());
+        Assert.assertNotNull(r.get());
 
+        System.gc();
+        Thread.sleep(100);
+
+        Assert.assertNull(r.get());
+        Assert.assertNotNull(sr.get());
+    }
+
+    @Test
+    public void softReferenceTest() throws Exception {
+        SoftReference r = new SoftReference(new String("soft string reference"));
+        Assert.assertNotNull(r.get());
+
+        System.gc();
+        Thread.sleep(100);
+
+        // Here should make a situation that memory may go drain.
+        // and then we can assert the soft referenced object be released
+//        Assert.assertNull(r.get());
     }
 }
